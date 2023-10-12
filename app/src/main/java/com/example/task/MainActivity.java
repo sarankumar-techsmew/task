6d7f2832d70ea9515a873290cfb7d8387f6efa54
Admin <sarankumar@techsmew.com>
2023-10-12 12:11:33 +0530

    finished project

---
+++ app/src/main/java/com/example/task/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.task;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.support.v4.app.RemoteActionCompatParcelizer;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+}
